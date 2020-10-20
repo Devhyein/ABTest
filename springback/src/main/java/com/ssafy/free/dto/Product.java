852d63bd059f825e2ff28d7b5a5aaad144c04528
rain151112@naver.com <rain151112@naver.com>
2020-10-20 14:18:23 +0900

    [Feat] Product Dto 생성 생성 : Product.java

---
+++ springback/src/main/java/com/ssafy/free/dto/Product.java
@@ -0,0 +1,87 @@
+package com.ssafy.free.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_no")
+    private int product_no; 
+
+    @Column(name = "product_name")
+    private String product_name;
+
+    @Column(name = "product_price")
+    private int product_price;
+
+    @Column(name = "product_detail")
+    private int product_detail;
+
+    @Column(name = "product_image")
+    private int product_image;
+
+    public Product() {
+    }
+
+    public Product(int product_no, String product_name, int product_price, int product_detail, int product_image) {
+        this.product_no = product_no;
+        this.product_name = product_name;
+        this.product_price = product_price;
+        this.product_detail = product_detail;
+        this.product_image = product_image;
+    }
+
+    public int getProduct_no() {
+        return product_no;
+    }
+
+    public void setProduct_no(int product_no) {
+        this.product_no = product_no;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public int getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(int product_price) {
+        this.product_price = product_price;
+    }
+
+    public int getProduct_detail() {
+        return product_detail;
+    }
+
+    public void setProduct_detail(int product_detail) {
+        this.product_detail = product_detail;
+    }
+
+    public int getProduct_image() {
+        return product_image;
+    }
+
+    public void setProduct_image(int product_image) {
+        this.product_image = product_image;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [product_detail=" + product_detail + ", product_image=" + product_image + ", product_name="
+                + product_name + ", product_no=" + product_no + ", product_price=" + product_price + "]";
+    }
+
+}
