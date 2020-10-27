c9e04756367653e5d11af6c7e0964a8ebab521b8
박하은 <eun-8279@hanmail.net>
2020-10-27 10:00:50 +0900

    [Fix] 파일 정리 수정 : App.vue, index.js 삭제 : Home.vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
 import SampleA from '@/views/SampleA.vue'
 import Login from '../views/Login.vue'
@@ -10,10 +9,6 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },{
     path: '/main',
     name: 'Main',
     component: Main
