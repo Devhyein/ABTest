06d8c732fcaef386dc4bb72c385ad30986e55f49
유지민 <ujimini11@gmail.com>
2020-10-30 18:06:27 +0900

    [Feat]관리자 로그인 API 적용

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,8 +3,8 @@
       <div>
         <h2>LogIn</h2>
       </div>
-    <div>ID <input type="text" placeholder="아이디" /></div>
-    <div>PW <input type="password" placeholder="비밀번호" /></div>
+    <div>ID <input type="text" v-model = "id" placeholder="아이디" /></div>
+    <div>PW <input type="password" v-model = "pw" placeholder="비밀번호" /></div>
     <div>
       <button @click="join()">회원가입</button>
       <button @click="login()">로그인</button>
@@ -12,13 +12,37 @@
   </div>
 </template>
 <script>
+import API from "@/api/API";
+import swal from "sweetalert";
+
 export default {
-  data: () => {},
+  data: () => {
+    return{
+      id:"",
+      pw:""
+    }
+  },
   methods: {
     join() {
       this.$router.push("/join");
     },
-    login() {},
+    login() {
+      let data = {};
+      data.id = this.id;
+      data.pw = this.pw;
+      API.manageLogin(
+        data,
+        (res) => {
+          console.log(res);
+          swal("로그인 완료", "정상적으로 로그인 되었습니다.", "success");
+          this.$router.push("/main");
+        },
+        (err) => {
+          console.log(err);
+          swal("로그인 실패", "로그인에 실패하였습니다.", "error");
+        }
+      );
+    },
   },
 };
 </script>
