cc21294ad114ada2fd20ae0c88653c69f6a61207
rain151112 <rain151112@naver.com>
2020-11-02 18:53:58 +0900

    [Temp] 머지

--- springback/src/main/java/com/ssafy/free/controller/HelloController.java
+++ springback/src/main/java/com/ssafy/free/controller/HelloController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 
+//주석
 @CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/spring")
