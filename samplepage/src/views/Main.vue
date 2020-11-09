766499b03dadea4397b789bed935c575d7209d50
박하은 <eun-8279@hanmail.net>
2020-11-09 22:28:24 +0900

    [Feat] 샘플 페이지 렌더링 방식 틀 완성 수정 : Main.vue

--- samplepage/src/views/Main.vue
+++ samplepage/src/views/Main.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-
+    <SampleA />
+    <SampleB />
   </div>
 </template>
 
 <script>
-
+import SampleA from '@/components/SampleA.vue'
+import SampleB from '@/components/SampleB.vue'
 
 export default {
   name: 'Main',
-
+  components: {
+    SampleA,
+    SampleB,
+  }
 }
 </script>
 
