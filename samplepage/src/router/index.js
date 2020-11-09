7e98c1beb436825fb97803e37343d4322aa8af6b
machine.kmg <machine.kmg@gmail.com>
2020-11-09 19:07:55 +0900

    [fix]

---
+++ samplepage/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import SampleA from '@/views/SampleA.vue'
+import SampleB from '@/views/SampleB.vue'
+import Join from '@/views/Join.vue'
+import Login from '@/views/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/sample',
+    name: 'SampleA',
+    component: SampleA
+  },
+  {
+    path: '/sample/b',
+    name: 'SampleB',
+    component: SampleB
+  },
+  {
+    path: '/sample/join',
+    name: 'Join',
+    component: Join
+  },
+  {
+    path: '/sample/login',
+    name: 'Login',
+    component: Login
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
