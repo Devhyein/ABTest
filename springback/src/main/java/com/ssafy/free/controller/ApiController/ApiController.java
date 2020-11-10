4fad3bdc89fa639751d67db48c74c132e6a9614e
machine.kmg <machine.kmg@gmail.com>
2020-11-10 15:31:32 +0900

    [Fix] CORS - 2

--- springback/src/main/java/com/ssafy/free/controller/ApiController/ApiController.java
+++ springback/src/main/java/com/ssafy/free/controller/ApiController/ApiController.java
@@ -16,7 +16,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@CrossOrigin(origins = { "*" })
+@CrossOrigin(origins = "*" )
 @RestController
 @RequestMapping("/api")
 public class ApiController {
