264507fe4527ca51a04c8f3ed2ffee7be80bb69a
machine.kmg <machine.kmg@gmail.com>
2020-11-10 15:20:53 +0900

    [Fix] CORS - 1

---
+++ springback/src/main/java/com/ssafy/free/config/CorsConfing.java
@@ -0,0 +1,14 @@
+package com.ssafy.free.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfing implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
