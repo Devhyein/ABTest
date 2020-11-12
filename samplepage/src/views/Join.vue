aa3906567008afe6a391f712d2059a9abb39c929
유지민 <ujimini11@gmail.com>
2020-11-12 18:15:02 +0900

    [Fix] 회원가입 default data 변경

--- samplepage/src/views/Join.vue
+++ samplepage/src/views/Join.vue
@@ -92,8 +92,8 @@ export default {
   data() {
     return {
       encar,
-      test_no: 2,
-      page_type: "A",
+      test_no: this.$store.state.test.test_no,
+      page_type: this.$store.state.test.page_type,
       birth: "",
       form: {
         id: "",
@@ -169,8 +169,8 @@ export default {
           "-" +
           String(date.getDate());
       let data = {};
-      data.test_no = this.$store.state.test.test_no;
-      data.page_type =this.$store.state.test.page_type;
+      data.test_no = this.test_no;
+      data.page_type =this.page_type;
       data.join_date = join_date;
       data.id = this.form.id;
       data.pw = this.form.pw;
